51e1bd5fefba8d23299736e8120a30c8fec1c000
Shashwat Singh <shashwat123student@gmail.com>
2023-09-19 16:33:19 +0530

    Added Some Files

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
+cd test
+ cd week-9
  git pull origin master
  cd server
  pm2 kill
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "work-class.pem" ubuntu@ec2-52-66-240-179.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
